9ac919290b18ae23e5f86f425f8b41333a233d8a
verah <verahnyagaka101@gmail.com>
2022-07-20 22:11:56 +0300

    Assignment completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const DirectorsList = directors.map((director) => (
+    <div key={director.name}>
+      <h2 >{director.name}</h2>
+
+      <p>Movies:</p>
+      <ul>
+        {(director.movies).map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <>
+      <h1>Directors Page</h1>
+      {DirectorsList}
+    </>
+  );
 }
 
 export default Directors;
